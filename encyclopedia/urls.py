a3c3b1bf6afd521e3415c84618fc05de0c39b463
Gilbert Khoi Tran <gilbertktran@Gilberts-MacBook-Air.local>
2022-06-29 22:32:06 -0500

    made entry display and serch results, next on work is entry display html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("wiki/<str:title>", views.displayEntry, name = "display_entry")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.displayEntry, name = "display_entry"),
+    path("search-results", views.displayResults, name = "display_results"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from . import util
 import markdown2
@@ -8,6 +10,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+class EditText(forms.Form):
+    text = forms.CharField(label='', widget=forms.Textarea(attrs={
+      "placeholder": "Your entry goes here"
+    }))
+
 def displayEntry(request, title):
-    mdfile = 
-    return
+    if get_entry(title) is not None:
+        return render(request, "encyclopedia/entry.html", {
+            "title":title,
+            "content":markdown2.markdown(get_entry(title)),
+        })
+    return HttpResponseRedirect(reverse('encyclopedia:search_results'))
+
+def displayResults(request):
+    return render(request, "encyclopedia/search.html", {
+        "content":list_entries(),
+    })
